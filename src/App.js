9ffd11bc61082c0a127b78f58a0de2187a959612
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-05-25 23:30:32 +0200

    import modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 
 import './App.css';
+import {hands} from '@mediapipe/hands'
+import * as Hands from '@mediapipe/hands'
+import * as cam from '@mediapipe/camera_utils'
+import {useRef,useEffect} from 'react'
+import Webcam from 'react-webcam'
 
 function App() {
+  const webcamRef=useRef(null)
+  const canvasRef=useRef(null)
+  let camera=null
+
+  useEffect(()=>{
+    const hands1 = new hands({locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    }});
+  })
   return (
     <div>
-  <h1>hello</h1>
+ <Webcam ref={webcamRef} style={{
+position: 'absolute',
+marginRight:'auto',
+marginLeft:'auto',
+left: 0,
+right:0,
+textAlign: 'center',
+zIndex:9,
+width:640,
+height: 480,
+
+ }}/>
+
+ <canvas ref={canvasRef} style={{
+position: 'absolute',
+marginRight:'auto',
+marginLeft:'auto',
+left: 0,
+right:0,
+textAlign: 'center',
+zIndex:9,
+width:640,
+height: 480,
+
+ }} >
+ </canvas>
     </div>
   );
 }
