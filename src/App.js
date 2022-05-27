634f98b7f381ad0e9aed7935e8410c717442a35b
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-05-27 23:36:16 +0200

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,72 +1,109 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 
-import './App.css';
-import {hands} from '@mediapipe/hands'
-import * as Hands from '@mediapipe/hands'
-import * as cam from '@mediapipe/camera_utils'
-import {useRef,useEffect} from 'react'
-import Webcam from 'react-webcam'
-
-
+import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands";
+import React, { useRef, useEffect } from "react";
+import * as cam from "@mediapipe/camera_utils";
+import Webcam from "react-webcam";
+import {drawConnectors, drawLandmarks} from'@mediapipe/drawing_utils'
 function App() {
-  const webcamRef=useRef(null)
-  const canvasRef=useRef(null)
-  let camera=null
-  function onResults(results){
-    const videoWidth = webcamRef.current.video.videoWidth;
-    const videoHeight = webcamRef.current.video.videoHeight;}
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  useEffect(()=>{
-    const hands1 = new hands({locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-    }})
-    hands1.setOptions({
+  let camera = null;
+  function onResults(results) {
+    // const video = webcamRef.current.video;
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+console.log(videoHeight)
+    // Set canvas width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const canvasElement = canvasRef.current;
+    const canvasCtx = canvasElement.getContext("2d");
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                       {color: 'red', lineWidth: 5});
+        drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+      }
+    }
+    canvasCtx.restore();
+  }
+
+  useEffect(() => {
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+
+    hands.setOptions({
       maxNumHands: 2,
       modelComplexity: 1,
       minDetectionConfidence: 0.5,
       minTrackingConfidence: 0.5
     });
+
     hands.onResults(onResults);
-    if(typeof webcamRef.current!=='undefined' && webcamRef.current!==null){
-      camera = new cam.Camera(webcamRef.current.video,{
-        onFrame:async()=>{
-await hands1.send({image:webcamRef.current.video})
-        }, width:640, height:480})
-        camera.start()
-      }
-    
-  },[]);
+
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null
+    ) {
+      camera = new cam.Camera(webcamRef.current.video, {
+        onFrame: async () => {
+          await hands.send({ image: webcamRef.current.video });
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    }
+  }, [onResults]);
   return (
-    <div>
- <Webcam ref={webcamRef} style={{
-position: 'absolute',
-marginRight:'auto',
-marginLeft:'auto',
-left: 0,
-right:0,
-textAlign: 'center',
-zIndex:9,
-width:640,
-height: 480,
-
- }}/>
-
- <canvas ref={canvasRef} style={{
-position: 'absolute',
-marginRight:'auto',
-marginLeft:'auto',
-left: 0,
-right:0,
-textAlign: 'center',
-zIndex:9,
-width:640,
-height: 480,
-
- }} >
- </canvas>
-    </div>
+    <center>
+      <div className="App">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />{" "}
+        <canvas
+          ref={canvasRef}
+          className="output_canvas"
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        ></canvas>
+      </div>
+    </center>
   );
 }
 
-export default App;
-      
+export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Apps from'../src/Apps'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
